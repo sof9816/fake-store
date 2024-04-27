22174fd3ec7aabe70643962fa8d66b3bb6a0000e
Mustafa Naser <sof9816@gmail.com>
2024-04-27 10:37:10 +0300

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Mustafa Naser on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
